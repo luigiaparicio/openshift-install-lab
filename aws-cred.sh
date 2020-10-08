c3f2cea0f0a366f29a26ab80acc3804f06663183
Luigi <luis.pablo.aparicio@gmail.com>
2020-10-08 17:58:44 -0300

    Create aws-cred.sh

---
+++ aws-cred.sh
@@ -0,0 +1,11 @@
+export AWSKEY=<YOURACCESSKEY>
+export AWSSECRETKEY=<YOURSECRETKEY>
+export REGION=us-east-2
+
+mkdir $HOME/.aws
+cat << EOF >>  $HOME/.aws/credentials
+[default]
+aws_access_key_id = ${AWSKEY}
+aws_secret_access_key = ${AWSSECRETKEY}
+region = $REGION
+EOF
